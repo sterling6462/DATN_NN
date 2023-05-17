ca71ff630749ee05db5001919bb2c3ecaeb4009d
sterling6462 <nguyennhungoc810@gmail.com>
2023-05-17 14:56:26 +0700

    feat: page home

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,12 +1,62 @@
-import "./App.css";
-import Home from "./pages/home";
+import { SnackbarProvider } from 'notistack'
+import Home from 'pages/home'
+import { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import { ThemeProvider, getCookie, useAuthStore } from './components'
+import { PATH } from './constants/Paths'
+import Login from './pages/login'
+import Register from './pages/register'
 
 function App() {
+  const [isLogged, setIsLogged] = useState<boolean | undefined>(undefined)
+  const setAuth = useAuthStore((store) => store.setAuth)
+
+  useEffect(() => {
+    const accessToken = getCookie('access')
+    try {
+      const decodeToken = JSON.parse(atob(accessToken?.split('.')[1]))
+      setAuth(decodeToken)
+    } catch (e) {}
+
+    const hasAccess = Boolean(accessToken)
+    setIsLogged(hasAccess)
+  }, [setAuth])
+
+  if (isLogged === undefined) return <></>
+
   return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+    <ThemeProvider>
+      <SnackbarProvider
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        maxSnack={3}
+      >
+        <BrowserRouter>
+          {isLogged ? <AuthorizedRoutes /> : <UnAuthorizedRoutes />}
+        </BrowserRouter>
+      </SnackbarProvider>
+    </ThemeProvider>
+  )
 }
 
-export default App;
+function AuthorizedRoutes() {
+  return (
+    <Routes>
+      <Route path={PATH.HOME} element={<Home />} />
+      <Route path={PATH.LOGIN} element={<Login />} />
+      <Route path={PATH.REGISTER} element={<Register />} />
+    </Routes>
+  )
+}
+
+function UnAuthorizedRoutes() {
+  return (
+    <Routes>
+      <Route path={PATH.HOME} element={<Home />} />
+      <Route path={PATH.LOGIN} element={<Login />} />
+      <Route path={PATH.REGISTER} element={<Register />} />
+    </Routes>
+  )
+}
+
+export default App
--- fe/src/pages/home/index.tsx
+++ fe/src/pages/home/index.tsx
@@ -1,7 +1,76 @@
-import { Layout } from 'components'
+import img from 'assets/img/img.jpg'
+import clsx from 'clsx'
+import { CardHome, Layout } from 'components'
+import styles from './style.module.scss'
 
-const Home = () => {
-  return <Layout>Home</Layout>
+const Data = [
+  {
+    id: 1,
+    imgSrc: img,
+    descTitle: 'A.101',
+    location: 'Da Nang',
+    roomAvailable: 5,
+    fees: '3.000.000đ',
+    descriptions:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam porro possimus architecto, ipsum voluptates rerum odit officia, labore illo fugiat neque similique tempore esse sed alias vel exercitationem aut cupiditate.'
+  },
+  {
+    id: 2,
+    imgSrc: img,
+    descTitle: 'A.102',
+    location: 'Da Nang',
+    roomAvailable: 5,
+    fees: '3.000.000đ',
+    descriptions:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam porro possimus architecto, ipsum voluptates rerum odit officia, labore illo fugiat neque similique tempore esse sed alias vel exercitationem aut cupiditate.'
+  },
+  {
+    id: 3,
+    imgSrc: img,
+    descTitle: 'A.103',
+    location: 'Da Nang',
+    roomAvailable: 5,
+    fees: '3.000.000đ',
+    descriptions:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam porro possimus architecto, ipsum voluptates rerum odit officia, labore illo fugiat neque similique tempore esse sed alias vel exercitationem aut cupiditate.'
+  },
+  {
+    id: 4,
+    imgSrc: img,
+    descTitle: 'A.104',
+    location: 'Da Nang',
+    roomAvailable: 5,
+    fees: '3.000.000đ',
+    descriptions:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam porro possimus architecto, ipsum voluptates rerum odit officia, labore illo fugiat neque similique tempore esse sed alias vel exercitationem aut cupiditate.'
+  },
+  {
+    id: 5,
+    imgSrc: img,
+    descTitle: 'A.105',
+    location: 'Da Nang',
+    roomAvailable: 5,
+    fees: '3.000.000đ',
+    descriptions:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam porro possimus architecto, ipsum voluptates rerum odit officia, labore illo fugiat neque similique tempore esse sed alias vel exercitationem aut cupiditate.'
+  }
+]
+
+export default function Home() {
+  return (
+    <Layout post>
+      {/* <SearchOptionsHome /> */}
+      <section className={clsx(styles.main, 'container', 'section')}>
+        <h3 data-aos="fade-right" className={styles.title}>
+          Recently viewed rooms
+        </h3>
+
+        <div className={clsx(styles.secContent, styles.grid)}>
+          {Data.map((item, index) => {
+            return <CardHome {...item} />
+          })}
+        </div>
+      </section>
+    </Layout>
+  )
 }
-
-export default Home
